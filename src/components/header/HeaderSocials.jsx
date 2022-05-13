c15b956d106d0b9c04cb11e4d7aea6b3bb4ed6fc
Tiago Batista <tiago.dev96@gmail.com>
2022-05-13 12:44:48 -0300

    instagram account change

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
-import {BsLinkedin, BsGithub, BsInstagram, BsWhatsapp} from 'react-icons/bs'
+import { BsLinkedin, BsGithub, BsInstagram, BsWhatsapp } from 'react-icons/bs'
 
 const HeaderSocials = () => {
   return (
     <div className="header__socials">
-        <a href="https://www.linkedin.com/in/tiagocb96/" target="__blank"><BsLinkedin/></a>
-        <a href="https://github.com/tiagodev96" target="__blank"><BsGithub/></a>
-        <a href="https://instagram.com/tfrontdev" target="__blank"><BsInstagram/></a>
-        <a href="https://wa.me/5571993553196" target="__blank"><BsWhatsapp/></a>
+      <a href="https://www.linkedin.com/in/tiagocb96/" target="__blank"><BsLinkedin /></a>
+      <a href="https://github.com/tiagodev96" target="__blank"><BsGithub /></a>
+      <a href="https://instagram.com/tiagocbatista" target="__blank"><BsInstagram /></a>
+      <a href="https://wa.me/5571993553196" target="__blank"><BsWhatsapp /></a>
     </div>
   )
 }
